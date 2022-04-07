6aa698989290942344c4aff774435d1b2e88e392
Luiz <luizhenriquefjs@hotmail.com>
2022-04-07 16:11:13 -0300

    Pasta com os aqruvos css, js, img

---
+++ public/js/listarprodutos.js
@@ -0,0 +1,23 @@
+const tabeltaProdutos = document.querySelector(".tabela__produtos");
+
+function pesquisar(){
+  const valorPesquisa = document.querySelector('#pesquisar').value;
+  const publivoAlvo = document.querySelectorAll('#publico-alvo');
+  nomesPublico = []
+  publivoAlvo.forEach(publico => {
+    nomesPublico.push(publico.textContent);
+  });
+
+  if(valorPesquisa == ""){
+    location.reload();
+  }
+
+  else if(nomesPublico.includes(valorPesquisa)){
+    publivoAlvo.forEach(texto => {
+      if (valorPesquisa != texto.textContent) {
+        let elementoPai = texto.parentElement;
+        elementoPai.remove();
+      }
+    })
+  }
+}
